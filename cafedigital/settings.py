e970005e7777e7eb1208802db7001f7995a5b10a
Henrique Rodrigues <ctm_rd@hotmail.com>
2024-11-12 23:50:42 -0300

    Update settings.py

--- cafedigital/settings.py
+++ cafedigital/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "cafe",
-    "list",
-    "rest_framework",
+    'cafe',
+    'list',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
