f25cc44310ab6eedd1de7b2b7d600de2933da9af
Henrique Rodrigues <ctm_rd@hotmail.com>
2024-11-13 14:56:33 -0300

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,2 @@
 pip install -r requirements.txt
+python3.12.5 manage.py collectstatic
--- cafedigital/settings.py
+++ cafedigital/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%3a)f)1!t*3by9*tddx55&9hg_mm8nr6*lvkca@p(ob^jc6&)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
